4dc9ba9eb8771609056bb232f4a6d4d170659752
houlianglv <houlianglv@163.com>
2016-06-18 19:22:04 -0400

    change min-search to cover more cases. Now could reach 2048 more easily

--- PlayerAI.py
+++ PlayerAI.py
@@ -9,50 +9,26 @@ infinity = 1.0e400
 
 
 class PlayerAI(BaseAI):
-    def __init__(self):
+    def __init__(self, t_limit):
         self.directionVectors = ((-1, 0), (1, 0), (0, -1), (0, 1))  # UP DOWN LEFT RIGHT
+        self.gradients = [[[0.135759, 0.121925, 0.102812, 0.099937],
+                           [0.09997992, 0.0888405, 0.076711, 0.0724143],
+                           [0.060654, 0.0562579, 0.037116, 0.0161889],
+                           [0.0125498, 0.00992495, 0.00575871, 0.00335193]],
+                          [[0.0125498, 0.060654, 0.09997992, 0.135759],
+                           [0.00992495, 0.0562579, 0.0888405, 0.121925],
+                           [0.00575871, 0.037116, 0.076711, 0.102812],
+                           [0.00335193, 0.0161889, 0.0724143, 0.099937]],
+                          [[0.00335193, 0.00575871, 0.00992495, 0.0125498],
+                           [0.0161889, 0.037116, 0.0562579, 0.060654],
+                           [0.0724143, 0.076711, 0.0888405, 0.09997992],
+                           [0.099937, 0.102812, 0.121925, 0.135759]],
+                          [[0.099937, 0.0724143, 0.0161889, 0.00335193],
+                           [0.102812, 0.076711, 0.037116, 0.00575871],
+                           [0.121925, 0.0888405, 0.0562579, 0.00992495],
+                           [0.135759, 0.09997992, 0.060654, 0.0125498]]]
         self.time_limit = None
-        self.current_max_tile = None
-        self.threshold = 256
-        self.snakes = [[[15, 14, 13, 12],
-                       [8, 9, 10, 11],
-                       [7, 6, 5, 4],
-                       [0, 1, 2, 3]],
-
-                       [[15, 8, 7, 0],
-                        [14, 9, 6, 1],
-                        [13, 10, 5, 2],
-                        [12, 11, 4, 3]],
-
-                       [[12, 13, 14, 15],
-                       [11, 10, 9, 8],
-                       [4, 5, 6, 7],
-                       [3, 2, 1, 0]],
-
-                       [[0, 7, 8, 15],
-                       [1, 6, 9, 14],
-                       [2, 5, 10, 13],
-                       [3, 4, 11, 12]],
-
-                       [[3, 4, 11, 12],
-                       [2, 5, 10, 13],
-                       [1, 6, 9, 14],
-                       [0, 7, 8, 15]],
-
-                       [[3, 2, 1, 0],
-                        [4, 5, 6, 7],
-                        [11, 10, 9, 8],
-                        [12, 13, 14, 15]],
-
-                       [[0, 1, 2, 3],
-                       [7, 6, 5, 4],
-                       [8, 9, 10, 11],
-                       [15, 14, 13, 12]],
-
-                       [[12, 11, 4, 3],
-                       [13, 10, 5, 2],
-                       [14, 9, 6, 1],
-                       [15, 8, 7, 0]]]
+        self.t_limit = t_limit
 
     @staticmethod
     def get_available_grid_cells(grid):
@@ -75,24 +51,14 @@ class PlayerAI(BaseAI):
     def within_bounds(grid, position):
         return 0 <= position[0] < grid.size and 0 <= position[1] < grid.size
 
-    def update_max_tile(self, grid):
-        if self.current_max_tile >= self.threshold:
-            return
-        else:
-            for x in [0, 1, 2, 3]:
-                for y in [0, 1, 2, 3]:
-                    if grid.map[x][y] >= self.threshold:
-                        self.current_max_tile = grid.map[x][y]
-                        return
-
     def getMove(self, grid):
-        best_move = -1
         # iterative_deepening with time limitation
-        self.time_limit = time.time() + 0.98
-
-        depth = 4
+        self.time_limit = time.time() + self.t_limit
+        best_move = -1
+        # depth = 4
+        depth = 1
         while time.time() < self.time_limit:
-            move_score = self.alpha_beta(grid, depth, -infinity, infinity, True)
+            move_score = self.search(grid, -infinity, infinity, depth, True)
             if move_score.direction is -1:
                 break
             best_move = move_score.direction
@@ -102,84 +68,91 @@ class PlayerAI(BaseAI):
             raise Exception("No move is provided!")
         return best_move
 
-    def alpha_beta(self, grid, depth, alpha, beta, player):
+    def search(self, grid, alpha, beta, depth, player):
+        # check the depth
         if time.time() >= self.time_limit:
             return MoveScore(-1, None)
+        if depth is 0:
+            return MoveScore(-1, self.eval(grid))
         if player:
-            children = []
-            for i in range(4):
-                new_grid = grid.clone()
-                if new_grid.move(i):
-                    children.append((i, new_grid))
-            if len(children) is 0:
-                # when player cannot move
-                return MoveScore(-1, self.eval(grid))
-            new_alpha = alpha
-            best_move = -1
-            for child in children:
-                direction = child[0]
-                child_grid = child[1]
-                if depth is 0:
-                    # if the depth is 0, evaluate the current board
-                    move_score = MoveScore(-1, self.eval(grid))
-                else:
-                    move_score = self.alpha_beta(child_grid, depth - 1, new_alpha, beta, False)
-                if move_score.score is None:
-                    # when reach the time limitation
-                    return move_score
-                if move_score.score > new_alpha:
-                    new_alpha = move_score.score
-                    best_move = direction
-                if new_alpha >= beta:
-                    break
-            return MoveScore(best_move, new_alpha)
+            return self.max_value(grid, alpha, beta, depth)
         else:
-            empty_cells = self.get_available_grid_cells(grid)
-            new_beta = beta
-            for cell in empty_cells:
-                # insert 2
-                grid.insertTile(cell, 2)
+            return self.min_value(grid, alpha, beta, depth)
+
+    def max_value(self, grid, alpha, beta, depth):
+        v = -infinity
+        move = -1
+        blocked_directions = 0
+        for direction in range(4):
+            new_grid = grid.clone()
+            if new_grid.move(direction):
                 if depth is 0:
-                    # when depth is 0, eval the current board
-                    new_beta = self.eval(grid)
-                else:
-                    new_beta = min(new_beta, self.alpha_beta(grid, depth - 1, alpha, new_beta, True).score)
-                grid.insertTile(cell, 0)
-                if new_beta <= alpha:
-                    return MoveScore(-1, new_beta)
-            for cell in empty_cells:
-                # insert 4
-                grid.insertTile(cell, 4)
-                if depth is 0:
-                    # when depth is 0, eval the current board
-                    new_beta = self.eval(grid)
-                else:
-                    new_beta = min(new_beta, self.alpha_beta(grid, depth - 1, alpha, new_beta, True).score)
-                grid.insertTile(cell, 0)
-                if new_beta <= alpha:
-                    return MoveScore(-1, new_beta)
-            return MoveScore(-1, new_beta)
+                    score = self.eval(grid)
+                    if score > v:
+                        v = score
+                        move = direction
+                    continue
+                # min_value returns a instance of MoveScore
+                move_score = self.search(new_grid, alpha, beta, depth - 1, False)
+                # check if the time limit is reached
+                if move_score.score is None:
+                    return move_score
+                if move_score.score > v:
+                    v = move_score.score
+                    move = direction
+                if v >= beta:
+                    return MoveScore(direction, v, cutoff=True)
+                if v > alpha:
+                    alpha = v
+            else:
+                # when you cannot move:
+                blocked_directions += 1
+        if blocked_directions is 4:
+            return MoveScore(-1, self.eval(grid))
+        else:
+            return MoveScore(move, v)
+
+    def min_value(self, grid, alpha, beta, depth):
+        v = infinity
+        cells = self.get_available_grid_cells(grid)
+        children = []
+        for cell in cells:
+            new_grid = grid.clone()
+            new_grid.insertTile(cell, 2)
+            children.append(new_grid)
+        for cell in cells:
+            new_grid = grid.clone()
+            new_grid.insertTile(cell, 4)
+            children.append(new_grid)
+
+        # search on each candidate
+        for child in children:
+            move_score = self.search(child, alpha, beta, depth - 1, True)
+            # check if time limit is reached
+            if move_score.score is None:
+                return move_score
+            if move_score.score < v:
+                v = move_score.score
+            if v <= alpha:
+                return MoveScore(-1, v, True)
+            beta = min(beta, v)
+
+        return MoveScore(-1, v)
 
     def eval(self, grid):
-        return self.snake_weight_score(grid)
-
-    def snake_weight_score(self, grid):
         max_score = None
-        for snake in self.snakes:
-            score = 0
+        for weight_matrix in self.gradients:
+            score = 0.0
             for x in range(4):
                 for y in range(4):
-                    # use left shift
-                    score += grid.map[x][y] << snake[x][y]
+                    score += grid.map[x][y] * weight_matrix[x][y]
             if score > max_score:
                 max_score = score
         return max_score
 
 
 class MoveScore:
-
     def __init__(self, direction, score, cutoff=False):
-
         self.direction = direction
         self.score = score
         self.cutoff = cutoff
